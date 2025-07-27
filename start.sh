37ffc5d6f98183cd683cde800552745293c5425a
unknown <endalkmin@gmail.com>
2025-07-28 01:26:20 +0300

    start unzip file

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker severity_predictor:app --bind 0.0.0.0:8000
 
+# Unzip model if needed
+if [ -f "tuned_voting_model.zip" ]; then
+    echo "[INFO] Extracting model from tuned_voting_model.zip..."
+    unzip -o tuned_voting_model.zip -d .
+fi
+
+# Launch FastAPI app with Gunicorn
+exec gunicorn severity_predictor:app --workers 1 --bind 0.0.0.0:8000
 
